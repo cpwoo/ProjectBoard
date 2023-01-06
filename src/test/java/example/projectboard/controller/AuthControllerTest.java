b432817b6f59e005a72c5f60de35f4d7d2e4db65
cpwoo <world9969@khu.ac.kr>
2023-01-06 19:51:04 +0900

    #24 - 로그인 테스트 문제 해결

--- src/test/java/example/projectboard/controller/AuthControllerTest.java
+++ src/test/java/example/projectboard/controller/AuthControllerTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @DisplayName("View Controller - 인증")
 @Import(SecurityConfig.class)
-@WebMvcTest
+@WebMvcTest(Void.class)
 public class AuthControllerTest {
 
     private final MockMvc mvc;
