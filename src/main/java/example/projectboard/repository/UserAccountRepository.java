1d014a581708058da763589e7e238dd0bd3a99b2
cpwoo <world9969@khu.ac.kr>
2023-01-07 19:58:35 +0900

    #37 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용

--- src/main/java/example/projectboard/repository/UserAccountRepository.java
+++ src/main/java/example/projectboard/repository/UserAccountRepository.java
@@ -3,5 +3,5 @@ package example.projectboard.repository;
 import example.projectboard.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
