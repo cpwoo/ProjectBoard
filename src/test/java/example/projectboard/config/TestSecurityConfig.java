20b95a60df7a6c7274a5b901a62095296114c85f
cpwoo <world9969@khu.ac.kr>
2023-01-08 22:00:25 +0900

    #45 - 시큐리티 설정과 자바 대응 코드 구현

---
+++ src/test/java/example/projectboard/config/TestSecurityConfig.java
@@ -0,0 +1,30 @@
+package example.projectboard.config;
+
+import example.projectboard.domain.UserAccount;
+import example.projectboard.repository.UserAccountRepository;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@Import(SecurityConfig.class)
+public class TestSecurityConfig {
+
+    @MockBean private UserAccountRepository userAccountRepository;
+
+    @BeforeTestMethod
+    public void securitySetup() {
+        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+                "steveTest",
+                "pw",
+                "steve-test@gmail.com",
+                "steve-test",
+                "steve memo"
+        )));
+    }
+
+}
