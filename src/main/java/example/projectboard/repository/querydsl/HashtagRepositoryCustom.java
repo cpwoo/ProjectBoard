a5440418bc7ebd753e50992867b80c4f265651dd
cpwoo <world9969@khu.ac.kr>
2023-01-30 19:02:45 +0900

    #57 - 도메인 변경점의 반영: 해시태그
    해시태그 엔티티가 추가되고, 게시글이 해시태그를 여러 개
    가질 수 있도록 설계가 변경됨.
    이 때문에 변경점이 크게 전파되었다.
    커밋이 컴파일 에러 상태로 두는 것은 좋지 않아서
    어쩔 수 없이 변경점을 모두 한 커밋에 담는데
    이 방법이 괜찮을지는 추후 더 고민해보자.
    
    그러나 도메인 변경만 반영하고 기능을 구현하지 않았으므로
    컴파일 에러만 잠시 피할 뿐, 테스트는 현재 실패함

---
+++ src/main/java/example/projectboard/repository/querydsl/HashtagRepositoryCustom.java
@@ -0,0 +1,7 @@
+package example.projectboard.repository.querydsl;
+
+import java.util.List;
+
+public interface HashtagRepositoryCustom {
+    List<String> findAllHashtagNames();
+}
---
+++ src/main/java/example/projectboard/repository/querydsl/HashtagRepositoryCustomImpl.java
@@ -0,0 +1,24 @@
+package example.projectboard.repository.querydsl;
+
+import example.projectboard.domain.Hashtag;
+import example.projectboard.domain.QHashtag;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class HashtagRepositoryCustomImpl extends QuerydslRepositorySupport implements HashtagRepositoryCustom {
+
+    public HashtagRepositoryCustomImpl() {
+        super(Hashtag.class);
+    }
+
+    @Override
+    public List<String> findAllHashtagNames() {
+        QHashtag hashtag = QHashtag.hashtag;
+
+        return from(hashtag)
+                .select(hashtag.hashtagName)
+                .fetch();
+    }
+
+}
